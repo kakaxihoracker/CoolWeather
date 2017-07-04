67d31f26b8359b5740a3d71ae1800039132a2f9c
horacker <xuzhengguang@huoqiu.cn>
2017-07-04 14:55:28 +0800

    添加数据库配置

---
+++ app/src/main/java/com/example/horacker/coolweather/db/County.java
@@ -0,0 +1,48 @@
+package com.example.horacker.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by horacker on 2017/7/4.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getWeatherId() {
+
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public String getCountyName() {
+
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+}
